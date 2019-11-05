45b3913b74d8521bdf667452932db2bc3939de9e
robsonsantos <robson.andrei@acad.pucrs.br>
2019-11-05 17:38:18 -0300

    # 0.1 Criacao das classes e arquivo inicial com tabuleiro

--- Java/src/Dominio/Tabuleiro.java
+++ Java/src/Dominio/Tabuleiro.java
@@ -1,4 +1,12 @@
 package Dominio;
 
 public class Tabuleiro {
+
+    public Tabuleiro(){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Teste ok!!");
+    }
 }
