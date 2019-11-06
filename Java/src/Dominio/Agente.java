cee2ee2e8478142b7b0e28682c03862ea59ba4d8
robsonsantos <robson.andrei@acad.pucrs.br>
2019-11-05 21:12:09 -0300

    # 0.2 Geracao da matriz com o tabuleiro

--- Java/src/Dominio/Agente.java
+++ Java/src/Dominio/Agente.java
@@ -1,4 +1,11 @@
 package Dominio;
 
 public class Agente {
+    private int coord_X;
+    private int coord_Y;
+//=> Importante o agente ter mapeado os seus movimentos no tabuleiro
+//=> para cima, baixo, esquerda e direita.
+
+
+
 }
--- Java/src/Dominio/Tabuleiro.java
+++ Java/src/Dominio/Tabuleiro.java
@@ -1,12 +1,81 @@
 package Dominio;
 
-public class Tabuleiro {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
-    public Tabuleiro(){
+public class Tabuleiro {
+    private String nome_Arquivo;
+    private String matriz_Tabeuleiro[][];
+    private int tamanho_matriz;
+
+
+    public Tabuleiro() {
+        this.nome_Arquivo = "labirinto1_10T2.txt";
+    }
+
+    public String[][] lerArqRetMatriz() throws FileNotFoundException {
+        String[][] aux = null;
+        int tam = 0;
+        Scanner in = new Scanner(new FileReader(nome_Arquivo));
+        tam = in.nextInt();
+        aux = new String[tam][tam];
+        in.close();
+        return aux;
+    }
+
+    public String[][] geraMatriz() throws IOException {
+        String[][] mat = this.lerArqRetMatriz();
+        String[] vetorString = new String[mat.length];
+
+        try {
+            Scanner in = new Scanner(new FileReader(nome_Arquivo));
+            in.nextLine();
+
+            int i;
+            while (in.hasNextLine()) {
+                for (i = 0; i < vetorString.length; ++i) {
+                    String line = in.nextLine().replace(" ", "");
+                    vetorString[i] = line;
+                }
+            }
+
+            for (i = 0; i < mat.length; ++i) {
+                for (int j = 0; j < mat.length; ++j) {
+                    char ch = vetorString[i].charAt(j);
+                    mat[i][j] = String.valueOf(ch);
+                }
+            }
+
+            in.close();
+        } catch (IOException var7) {
+            System.out.println(var7.getMessage());
+        }
+        this.matriz_Tabeuleiro = mat;
+        return mat;
+    }
+
+    public void printaMatriz() {
+        for (int i = 0; i < this.matriz_Tabeuleiro.length; ++i) {
+            for (int j = 0; j < this.matriz_Tabeuleiro.length; ++j) {
+                System.out.print(this.matriz_Tabeuleiro[i][j] + " ");
+            }
+
+            System.out.println();
+        }
 
     }
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
         System.out.println("Teste ok!!");
+        Tabuleiro tab = new Tabuleiro();
+        tab.lerArqRetMatriz();
+        String[][] matrix = tab.geraMatriz();
+        tab.printaMatriz();
+
+
     }
 }
