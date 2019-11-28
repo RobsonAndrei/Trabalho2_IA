d1221977bc8d264e120fd0476175048f38e1f120
robsonsantos <robson.andrei@acad.pucrs.br>
2019-11-28 18:03:53 -0300

    # 0.11 Teste para a primeira populacao e saida da rede ok

--- Java/src/Dominio/Agente.java
+++ Java/src/Dominio/Agente.java
@@ -4,6 +4,8 @@ import sun.tools.jconsole.Tab;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Agente {
@@ -33,6 +35,7 @@ public class Agente {
         String[][] matriz = tab.geraMatriz();
         String[] vet = new String[4];
         int ultimaPosicao = matriz.length - 1;
+
         coord_X = tab.getAgent_X();
         coord_Y = tab.getAgent_Y();
 
@@ -104,30 +107,50 @@ public class Agente {
 
     //      === Método conversor de vetor de String gerado pela matriz      ===
     //      === para um vetor de interios que ira ser usado na rede neural  ===
+
+    /***
+     *
+     * @param vet
+     * @return
+     *      === Codificação para a redeNural
+     *      *** 0 => 0      ==> posição vazia
+     *      *** 1 => 1      ==> parede
+     *      *** M => 2      ==> moeda
+     *      *** S => 3      ==> saída
+     *      *** null => 4   ==> posição nula
+     */
     public int[] convertVetor(String[] vet) {
         int[] vetorInt = new int[vet.length];
-        for (int i = 0; i < vet.length; i++) {
-            if (vet[i] == "0") {
-                vetorInt[i] = 0;
-            }
-            if (vet[i] == "1") {
-                vetorInt[i] = 1;
-            }
-            if (vet[i] == "M") {
-                vetorInt[i] = 2;
-            }
-            if (vet[i] == "S") {
-                vetorInt[i] = 3;
-            }
-            if (vet[i] == null) {
-                vetorInt[i] = 4;
+
+        for (int i = 0; i < 4; i++) {
+            if ("0".equals(vet[i])) vetorInt[i] = 0;
+            else {
+                if ("1".equals(vet[i])) {
+                    vetorInt[i] = 1;
+
+                } else {
+                    if ("M".equals(vet[i])) {
+                        vetorInt[i] = 2;
+
+                    } else {
+                        if ("S".equals(vet[i])) {
+                            vetorInt[i] = 3;
+
+                        } else {
+                            if (vet[i] == null) {
+                                vetorInt[i] = 4;
+                            }
+                        }
+                    }
+                }
             }
         }
+
 
         return vetorInt;
     }
 
-    public void AlgorGenetico() {
+    public void AlgorGenetico() throws IOException {
 
         /***
          *      ======================
@@ -147,29 +170,77 @@ public class Agente {
          */
         RedeNeural net = new RedeNeural();
         double[] vetPopulacaoInicial = new double[40];
-        for (int i = 0; i < vetPopulacaoInicial.length; i++) {
-            vetPopulacaoInicial[i] = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
-        }
+        List<double[]> vetPop = new ArrayList<>();
+        int pontos = 0;
+        int moedas = 0;
         int geracao = 0;
 
-        for (int i = 0; i < vetPopulacaoInicial.length; i++) {
-            System.out.printf("%.02f", vetPopulacaoInicial[i]);
-            System.out.println();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < vetPopulacaoInicial.length; j++) {
+                vetPopulacaoInicial[j] = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
+            }
+            vetPop.add(vetPopulacaoInicial);
         }
 
 
+//        for (int i = 0; i < vetPopulacaoInicial.length; i++) {
+//            System.out.printf("%.02f", vetPopulacaoInicial[i]);
+//            System.out.println();
+//        }
+
+        net.ajustePesos(vetPopulacaoInicial);
+        String[] vetPerc = percepcao();
+        int[] strToint = convertVetor(vetPerc);
+        double[] vetSaida = net.getSaida(strToint[0], strToint[1], strToint[2], strToint[3]);
+        System.out.println("========= Vetor de Saida da rede =========");
+        for (int i = 0; i < vetSaida.length; i++) {
+            System.out.println(vetSaida[i]);
+        }
+
+        int maioPosiVetorSaida = getMaioPosiVetorSaida(vetSaida);
+
+
+        System.out.println("============= Indicação da rede ==========");
+        if (maioPosiVetorSaida == 0) {
+            System.out.println("Indicação da rede: CIMA");
+        }
+        if (maioPosiVetorSaida == 1) {
+            System.out.println("Indicação da rede: DIREITA");
+        }
+        if (maioPosiVetorSaida == 2) {
+            System.out.println("Indicação da rede: ESQUERDA");
+        }
+        if (maioPosiVetorSaida == 3) {
+            System.out.println("Indicação da rede: BAIXO");
+        }
+        System.out.println("==========================================");
+
+
+    }
+
+    public int getMaioPosiVetorSaida(double[] vetSaida) {
+        double maior = 0.0;
+        int maiorint = -1;
+        for (int i = 0; i < vetSaida.length; i++) {
+            if (vetSaida[i] > maior) {
+                maior = vetSaida[i];
+                maiorint = i--;
+            }
+        }
+        return maiorint;
     }
 
 
     public static void main(String[] args) throws IOException {
         Agente ag = new Agente();
         String[] vetor = ag.percepcao();
+        System.out.println("========= Percepção do tabuleiro =========");
         for (int i = 0; i < vetor.length; i++) {
             System.out.println(vetor[i]);
         }
-        System.out.println();
-        int[] vetint = ag.convertVetor(vetor);
 
+        int[] vetint = ag.convertVetor(vetor);
+        System.out.println("===== Codificação entrada da rede: X =====");
         for (int i = 0; i < vetint.length; i++) {
             System.out.println(vetint[i]);
         }
